9d25672356297875eb5d848322e57055e34e62b5
vnix <nicholasbuchanan@pop-os.localdomain>
2021-12-26 23:52:20 -0600

    Created Channels and LiveView combo

--- lib/phraze_web/endpoint.ex
+++ lib/phraze_web/endpoint.ex
@@ -11,6 +11,9 @@ defmodule PhrazeWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/socket", PhrazeWeb.UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
