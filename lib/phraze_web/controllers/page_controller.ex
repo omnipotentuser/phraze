6bfa2867e81447a21e155f27755811c974d783d1
vnix <nicholasbuchanan@pop-os.localdomain>
2021-12-30 18:27:43 -0600

    adding live page

--- lib/phraze_web/controllers/page_controller.ex
+++ lib/phraze_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule PhrazeWeb.PageController do
   use PhrazeWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # NICK - If we want to render plain index
+    # render(conn, "index.html")
+    conn
+    |> redirect(to: Routes.room_new_path(conn, :new))
   end
 end
